63b30374a3646d3e794b268b8585bc9cde8e7886
Kenneth Naumann <kdnt3c@mst.edu>
2021-01-30 12:11:28 -0600

    added error catch in app.js and clean script in package.json

--- app.js
+++ app.js
@@ -39,11 +39,13 @@ connection.connect();
 let sql_files = [];
 
 // Initial connection to receive current mySQL table
-connection.query('SELECT * FROM files', function (req, res) {
-  var res_string = JSON.stringify(res);
-  var res_json = JSON.parse(res_string);
+connection.query('SELECT * FROM files', function (error, req, res) {
+  if (!error) {
+    var res_string = JSON.stringify(res);
+    var res_json = JSON.parse(res_string);
 
-  sql_files = res_json;
+    sql_files = res_json;
+  }
 });
 
 // Gets that the user connected

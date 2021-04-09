3fb305940271666b718e5cc0c842572e917d23e6
Kenneth Naumann <kdnt3c@mst.edu>
2021-04-09 00:13:07 -0500

    Added more documentation for backend of our senior design project, almost ready for the final presentation

--- app.js
+++ app.js
@@ -14,9 +14,22 @@ const mysql = require('mysql');
 // TODO: local caching
 // let useMySQL = true;
 
-// Arduino control variables
-// const SerialPort  = require('serialport');
-// const serial_port = new SerialPort('/dev/ttyACM0');
+// Arduino control variables (port and position of file)
+const SerialPort  = require('serialport');
+const serial_port = new SerialPort('COM4');
+let pos = 0;
+
+let sql_files = [];
+
+// create arrays and max positions available
+let rows = new Array(2).fill([]);
+const MAX_POS = 3;
+
+// push that all positions are available
+for(let i = 0; i < MAX_POS; i++)
+{
+  rows[0].push(i);
+}
 
 
 // Setups socket.io to run across the http server and be setup to accept requests from localhost:3000 origin
@@ -55,8 +68,6 @@ connection.connect(function (error) {
 });
 
 
-let sql_files = [];
-
 console.log("Calling SELECT on " + DB_TABLE + " table.");
 
 // Initial connection to receive current mySQL table
@@ -69,6 +80,15 @@ connection.query('SELECT * FROM ' + DB_TABLE, function (error, res) {
   let res_string = JSON.stringify(res);
 
   sql_files = JSON.parse(res_string);
+
+  let len = sql_files.length;
+
+  for(let i = 0; i < len; i++) {
+    rows[0] = rows[0].filter(function (item) {
+      return item !== sql_files[i].pos;
+    });
+  }
+
 });
 
 // Gets that the user connected
@@ -93,91 +113,111 @@ io.on('connection', (socket) => {
 
   // Inserts file object into the mySQL table
   socket.on('insert file', (file) => {
-    // TODO: POST to Arduino
-    console.log('Inserting file.');
 
-    const mysql_arr = [[file.name, file.category, file.date,file.creation_date ,file.uuid]];
+    console.log("inserting file");
 
-    connection.query("INSERT INTO " + DB_TABLE + " (name, category, date, creation_date, uuid) VALUES (?)", mysql_arr, (error) => {
+    let drawer = file.drawer - 1;
+    let len = rows[drawer].length;
+    let mode = "0";
+
+    // calculate rows and position based on drawer
+
+    if (len !== 0) {
+      pos = rows[0][0];
+    } else {
+      return console.error("DRAWER FULL");
+    }
+
+    file["pos"] = pos;
+
+    // create array with all of data in it for the row
+    const mysql_arr = [[file.name, file.category, file.date, file.creation_date, file.uuid, file.drawer, pos]];
+
+
+    // Insert query on to database
+    connection.query("INSERT INTO " + DB_TABLE + " (name, category, date, creation_date, uuid, drawer, pos) VALUES (?)", mysql_arr, (error) => {
       if (error) {
-        return console.error("Error on INSERT INTO on " + DB_TABLE + " table!");
-      }
-
-      console.log('File successfully inserted.');
-
-      sql_files.push(file);
-      io.sockets.emit('receive files', sql_files);
-    });
-
-    /* TODO: add back for arduino
-    serial_port.write('insert', function(serial_error) {
-
-      if (serial_error) {
-        return console.error("Error on INSERT function on serial connection");
+        return console.log("Error on INSERT INTO on " + DB_TABLE + " table!");
       }
 
       //  serial connection successful insert into database
+      let serial_string = mode + "\n" + pos.toString() + "\n";
+      console.log(serial_string);
+      serial_port.write(serial_string, function (serial_error) {
 
-      connection.query("INSERT INTO " + DB_TABLE + " (name, category, date, uuid) VALUES (?)", mysql_arr, (error) => {
-        if (error) {
-          return console.error("Error on INSERT INTO on " + DB_TABLE + " table!");
+        if (serial_error) {
+          return console.log("Error on INSERT function on serial connection");
         }
+
 
         console.log('File successfully inserted.');
 
+        // push to sql_files array, remove position from available positions and emit sql_files
         sql_files.push(file);
+
+        rows[0] = rows[0].filter(item => item !== pos);
         io.sockets.emit('receive files', sql_files);
       });
     });
-    */
-
   });
 
   // Deletes file that has uuid
   socket.on('delete file', (uuid) => {
-    // TODO: DELETE to Arduino
-    console.log("Deleting file with uuid: ", uuid);
 
+    // SQL statement prep
     const SQL = "DELETE FROM " + DB_TABLE + " WHERE uuid = ?";
 
-    connection.query(SQL, uuid, (error) => {
+    let sql_file = sql_files.find(element => element.uuid === uuid);
+    pos = sql_file.pos;
+    const drawer = sql_file.drawer;
+    const mode = "0";
+
+    connection.query(SQL, uuid, (error, results) => {
       if (error) {
-        return console.error("Error on DELETE FROM on " + DB_TABLE + " table!");
+        return console.log("Error on DELETE FROM on " + DB_TABLE + " table!");
       }
 
-      console.log("File successfully deleted with uuid: ", uuid);
+      // prep serial string for writing to serial
+      let serial_string = mode + "\n" + pos.toString() + "\n";
 
-      // Filters sql_files to get rid of object with given uuid
-      sql_files = sql_files.filter(function (e) {
-        return e.uuid !== uuid
-      });
-
-      io.sockets.emit('receive files', sql_files);
-
-      /* TODO: add back for arduino
-      serial_port.write('delete', function(serial_error) {
-
-
+      serial_port.write(serial_string, function(serial_error) {
         if (serial_error) {
-          return console.error("Error on DELETE function on serial connection");
+          return console.log("Error on DELETE function on serial connection");
         }
 
-        connection.query(SQL, uuid, (error) => {
-          if (error) {
-            return console.error("Error on DELETE FROM on " + DB_TABLE + " table!");
-          }
+        console.log("File successfully deleted with uuid: ", uuid);
 
-          console.log("File successfully deleted with uuid: ", uuid);
+        // Filters sql_files to get rid of object with given uuid
+        sql_files = sql_files.filter(function (e) {
+          return e.uuid !== uuid
+         });
 
-          // Filters sql_files to get rid of object with given uuid
-          sql_files = sql_files.filter(function (e) {
-            return e.uuid !== uuid
-          });
 
-          io.sockets.emit('receive files', sql_files);
-        });
+        // push position back to rows and sort!
+        rows[0].push(pos);
+        rows[0].sort();
+
+        // emit new sql_files to front end for user consumption
+        io.sockets.emit('receive files', sql_files);
       });
-      */
+    });
+  });
+
+  socket.on('close', () => {
+    // set mode to close mode
+    let mode = "1";
+    // grab led position
+    let led = pos.toString();
+
+    // create serial string
+    let serial_str = mode + '\n' + led + '\n';
+
+    serial_port.write(serial_str, function (serial_error) {
+      if(serial_error){
+        return console.log(serial_error);
+      }
+
+      console.log('closing drawer');
 
     });
   });
